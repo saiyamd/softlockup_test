47e95cb806f77858bb9aa8f7c0fe8d7a4323f042
Saiyam Doshi <saiyamdoshi.in@gmail.com>
2015-12-29 12:18:17 +0530

    Test module code to generate CPU soft lockup/panic.
    - Busy loop in critical section hog CPU0 and does not relinquish it for other processes.
    - To generate panic on soft lock-up: enable "CONFIG_BOOTPARAM_SOFTLOCKUP_PANIC" in kernel configuration

---
+++ softlockup_test.c
@@ -0,0 +1,52 @@
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/kthread.h>
+
+struct task_struct *task0;
+static spinlock_t spinlock;
+int val;
+
+int task(void *arg)
+{
+    printk(KERN_INFO "%s:%d\n",__func__,__LINE__);
+    /* To generate panic uncomment following */
+    /* panic("softlockup: hung tasks"); */
+
+    while(!kthread_should_stop()) {
+        printk(KERN_INFO "%s:%d\n",__func__,__LINE__);
+        spin_lock(&spinlock);
+        /* busy loop in critical section */
+        while(1) {
+            printk(KERN_INFO "%s:%d\n",__func__,__LINE__);
+        }
+
+        spin_unlock(&spinlock);
+    }
+
+    return val;
+}
+
+static int softlockup_init(void)
+{
+    printk(KERN_INFO "%s:%d\n",__func__,__LINE__);
+
+    val = 1;
+    spin_lock_init(&spinlock);
+    task0 = kthread_run(&task,(void *)val,"softlockup_thread");
+    set_cpus_allowed(task0, *cpumask_of(0));
+
+    return 0;
+}
+
+static void softlockup_exit(void)
+{
+    printk(KERN_INFO "%s:%d\n",__func__,__LINE__);
+    kthread_stop(task0);
+}
+
+module_init(softlockup_init);
+module_exit(softlockup_exit);
+
+MODULE_AUTHOR("Saiyam Doshi");
+MODULE_DESCRIPTION("Test module to generate CPU soft lock-up/panic");
+MODULE_LICENSE("GPL v2");
